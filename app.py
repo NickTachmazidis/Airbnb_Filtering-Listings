bb960e41ba3531c99b75ab20a19d07b50f3216b7
NickTachmazidis <110339671+NickTachmazidis@users.noreply.github.com>
2022-12-18 11:26:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,7 @@ from pandas.api.types import (
     is_object_dtype
 )
 
-st.title("Filter your Airbnb Listings dataframe!")
-
-st.write(
-    """This app is based on this blog [here](https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/). 
-    Can you think of ways to extend it with visuals?
-    """
-)
-
+st.title("Filter the Airbnb Listings Dataframe")
 
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
